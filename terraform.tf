5e98e4b7b39071313f1c92a12dc3a41ca6cb86d4
William Bradley <williamabradley@outlook.com>
2021-02-06 15:24:31 +1300

    Added lambda_archive output, in case you need to chain the resource.

--- terraform.tf
+++ terraform.tf
@@ -201,3 +201,8 @@ output "function_arn" {
   value       = aws_lambda_function.ingestion_function.arn
   description = "Log ingestion lambda function ARN"
 }
+
+output "lambda_archive" {
+  depends_on = [null_resource.build_lambda]
+  value      = local.archive_name
+}
